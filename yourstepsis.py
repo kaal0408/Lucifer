96318d31df3785017934c4831c36ebce3de77e22
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-09-12 22:03:51 +0530

    Update yourstepsis.py

--- yourstepsis.py
+++ yourstepsis.py
@@ -1,6 +1,6 @@
 from os import system
-system("git clone https://gitHub.com/kaal0408/Lucifer-X && cd Lucifer-X && python3 -m Lucifer")
+system("git clone https://gitHub.com/kaal0408/Lucifer && cd Lucifer && python3 -m Lucifer")
 # if got error
-print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @LuciferXsupport")
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @Lucifer_support_group")
 # Script written for stop heroku suspension
 # use with credit else gay
