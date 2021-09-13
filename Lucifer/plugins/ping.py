6d6f6ec126353ec9f5aa567dabcd8225063b98a6
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-09-13 21:43:07 +0530

    Update ping.py

--- Lucifer/plugins/ping.py
+++ Lucifer/plugins/ping.py
@@ -1,13 +1,57 @@
+# Re-written for TeleBot by @Murat_30_God
+
+import time
 from datetime import datetime
 
+from Lucifer import CMD_HELP
+from Lucifer.__init__ import StartTime
+from Lucifer.plugins import OWNER_ID, LUCIFER_NAME
 
-@asst_cmd("ping$")
-@owner
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+
+# @command(pattern="^.ping$")
+
+
+@telebot.on(admin_cmd(pattern="ping$"))
+@telebot.on(sudo_cmd(pattern="ping$", allow_sudo=True))
 async def _(event):
+    if event.fwd_from:
+        return
     start = datetime.now()
+    x = await eor(event, "⛝ Pong! ⛝")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await asst.send_message(
-        event.chat_id,
-        f"**Pong!!**\n `{ms} milliseconds`",
+    uptime = get_readable_time((time.time() - StartTime))
+    await x.edit(
+        f"⪼ **Ping speed** : `{ms}`\n⪼ **Uptime** : `{uptime}`\n⪼ **Owner** : [{LUCIFER_NAME}](tg://user?id={OWNER_ID})"
     )
+
+
+CMD_HELP.update({"ping": ".ping\nUse - See the ping stats and uptime of userbot."})
