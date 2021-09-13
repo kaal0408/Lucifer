ff4f411151a682bc0024861dd6487c53990b2f5d
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-09-13 21:19:03 +0530

    Create ping.py

---
+++ Lucifer/plugins/ping.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+
+@asst_cmd("ping$")
+@owner
+async def _(event):
+    start = datetime.now()
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await asst.send_message(
+        event.chat_id,
+        f"**Pong!!**\n `{ms} milliseconds`",
+    )
