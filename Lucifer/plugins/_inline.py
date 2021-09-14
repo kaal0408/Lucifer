e3a0d9300f8dd81de63dea62b6b1728f31a50659
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-09-14 13:01:56 +0530

    Update _inline.py

--- Lucifer/plugins/_inline.py
+++ Lucifer/plugins/_inline.py
@@ -328,7 +328,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             else:
                 reply_pop_up_alert = help_string
             reply_pop_up_alert += "\n Use .unload {} to remove this plugin\n\
-                © Telebot".format(
+                ©Luciferbot".format(
                 plugin_name
             )
             if len(help_string) >= 140:
