e58b85698f6b7b3b3c434b09915fa6c6f479d04a
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-09-14 12:13:12 +0530

    Update __init__.py

--- Lucifer/plugins/__init__.py
+++ Lucifer/plugins/__init__.py
@@ -2,7 +2,7 @@ from telethon.tl.types import Channel
 
 from Lucifer import *
 from Lucifer import ALIVE_NAME, bot, luciferver
-from LuciferConfig import Config, Var
+from Lucifer.LuciferConfig import Config, Var
 
 # stats
 if Var.PRIVATE_GROUP_ID:
