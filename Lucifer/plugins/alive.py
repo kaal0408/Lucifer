eb729ba677399f207cfcc73df183afe7628f345f
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-09-19 10:48:50 +0530

    Update alive.py

--- Lucifer/plugins/alive.py
+++ Lucifer/plugins/alive.py
@@ -20,8 +20,7 @@ CUSTOM_ALIVE = (
     if Var.CUSTOM_ALIVE
     else "**Your LuciferX in working.**"
 )
-ALV_PIC = Var.ALIVE_PIC if Var.ALIVE_PIC
-    else "https://telegra.ph/file/73373552e9217e010e853.jpg"
+ALV_PIC = Var.ALIVE_PIC if Var.ALIVE_PIC else "https://telegra.ph/file/73373552e9217e010e853.jpg"
 )
 
 if Config.SUDO_USERS:
