eab442f76a4c42723b487e8b5fbab76d2f80b836
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-09-22 12:45:07 +0530

    Update pmsecurity.py

--- Lucifer/plugins/pmsecurity.py
+++ Lucifer/plugins/pmsecurity.py
@@ -13,7 +13,7 @@ from telethon.tl.functions.users import GetFullUserRequest
 import Lucifer.plugins.sql_helper.pmpermit_sql as pmpermit_sql
 from Lucifer import CMD_HELP, CUSTOM_PMPERMIT, bot
 from Lucifer.utils import admin_cmd
-from Lucifer.Luciferconfig import Config,var
+from Lucifer.Luciferconfig import Config, var
 
 NAME = Config.NAME
 PM_PIC = os.environ.get("PM_PIC", None)
